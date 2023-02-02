443011d4c3ce6aebd3ac187055bc35a420b4bc3a
anetochkadmi <anyamyliferulitforever@gmail.com>
2023-02-02 17:09:23 +0200

    add search login form, register form and url

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,5 @@
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -14,4 +15,12 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
+    
     page.should_be_login_link()
+    page.go_to_login_page()
+    
+    login_page = LoginPage(browser, link)
+    login_page.should_be_login_page()
+    # login_page.should_be_login_form()
+    # login_page.should_be_register_form()
+    # login_page.should_be_login_url()
