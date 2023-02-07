6ede2a65ace863b0b6ea42cc0906b59180acc6a9
anetochkadmi <anyamyliferulitforever@gmail.com>
2023-02-07 16:00:44 +0200

    добавлен переход к странице логина

--- test_main_page.py
+++ test_main_page.py
@@ -12,15 +12,9 @@ def go_to_login_page(browser):
     login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    
-    page.should_be_login_link()
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
     page.go_to_login_page()
-    
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
-    # login_page.should_be_login_form()
-    # login_page.should_be_register_form()
-    # login_page.should_be_login_url()
